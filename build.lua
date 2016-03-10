ab70745ad2e0787374329ab86bafd2036a3d447a
Joseph Wright <joseph.wright@morningstar2.co.uk>
2016-03-10 21:51:40 +0000

    Set up auto-versioning

--- build.lua
+++ build.lua
@@ -27,6 +27,24 @@ packtdszip  = true
 -- No tests for this bundle
 testfildir = ""
 
+versionfiles = {"*.bbx", "*.cbx", "*.tex"}
+
+function setversion_update_line (line, date, version)
+  local date = string.gsub(date, "%-", "/")
+  if string.match(line, "Released %d%d%d%d/%d%d/%d%d") or
+     string.match(line, "last revised %d%d%d%d/%d%d/%d%d") then
+    line = string.gsub(line, "%d%d%d%d/%d%d/%d%d", date)
+  end
+  if string.match(line, "This file describes v") then
+    line = string.gsub(line, "v%d%.%d%w?", "v" .. version)
+  end
+  if string.match(line, "^\\ProvidesFile") then
+    line = string.gsub(line, "%d%d%d%d/%d%d/%d%d", date)
+    line = string.gsub(line, "v%d%.%d%w?", "v" .. version)
+  end
+  return line
+end
+
 -- Find and run the build system
 kpse.set_program_name ("kpsewhich")
 dofile (kpse.lookup ("l3build.lua"))
